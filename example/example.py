d40e5067fdb1c69fb205cbd7958f4707e5271385
Dominic A. Sirianni <sirianni.dom@gmail.com>
2017-08-04 13:50:23 -0400

    Fixes spelling/syntax error in loop example

--- example/example.py
+++ example/example.py
@@ -11,4 +11,4 @@ geometry.write_input(file_name = 'hello_world.in', solv_mon = 'B')
 ##to put solvent in C by default where all solvent is water for list of geometries 
 #for geometry in geometry_list:
 #    this_geom = geom(geometry, solvent = 'HOH')
-#    this_geom.write_ipnut(file_name = geometry.split('.xyz')[0]+'.in', solv_mon = 'C')
+#    this_geom.write_input(file_name = geometry.split('.xyz')[0]+'.in', solv_mon = 'C')
